af5e81b41e29275a41ad740b23774ca0c4b03878
Isao Kanda <isao.kanda@n-kishou.co.jp>
2021-03-06 17:26:25 +0900

    appended date to version information

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-FLAG="-s -w -X main.version=$(git tag | sort -V | tail -1)"
+FLAG="-s -w -X main.version=$(git tag | sort -V | tail -1)-$(date +%Y%m%d)"
 
 #go build "$FLAG"
 go build -ldflags "$FLAG" 
